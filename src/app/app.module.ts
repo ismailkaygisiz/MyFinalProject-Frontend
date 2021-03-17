0ad7b1b0db86e989dd02ebcd7a3bc56a82561506
ismailkaygisiz <kaygisizismail24@gmail.com>
2021-03-17 22:22:34 +0300

    Pipes Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
-import { NaviComponent } from './components/navi/navi.component'; 
+import { NaviComponent } from './components/navi/navi.component';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+import { FormsModule } from '@angular/forms';
+import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 @NgModule({
   declarations: [
@@ -13,8 +16,10 @@ import { NaviComponent } from './components/navi/navi.component';
     ProductComponent,
     CategoryComponent,
     NaviComponent,
+    VatAddedPipe,
+    FilterPipePipe,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
